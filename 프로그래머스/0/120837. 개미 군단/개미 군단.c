853c3b157f0791ce5948e4d8b89a7ad9cb84ba37
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-08-18 20:52:28 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int hp) {
+    int answer = 0;
+    
+    int high = 5;
+    int middle = 3;
+    int bottom = 1;
+    
+    int quo = hp / high;
+    int remainder = hp % high;
+    
+    if(remainder == 0)
+    {
+        answer = quo;
+        return answer;
+    }
+    else
+    {
+        answer = quo;
+        quo = remainder / middle;
+        remainder = remainder % middle;
+        
+        if(remainder == 0)
+        {
+            answer = answer + quo;
+            return answer;
+        }
+        else
+        {
+            answer = answer + quo;
+            quo = remainder / bottom;
+            remainder = remainder % bottom;
+            
+            if(remainder == 0)
+            {
+                answer = answer + quo;
+                return answer;
+            }
+            else
+            {
+                answer = answer + quo;
+                return answer;
+                
+            }
+        }
+    }
+    
+    return answer;
+}
