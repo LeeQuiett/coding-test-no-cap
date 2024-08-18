aaf7197cbbb4a97eb1f201164694935bc219cf98
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-08-18 19:08:40 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.02 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string, int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc( (sizeof(my_string) - 1) * n + 1);
+    
+    int sizeOfArray =(sizeof(my_string) - 1) * n + 1;
+    int cnt = 0;
+    
+    char tempAnswer[sizeOfArray];
+    
+    
+    
+    for(int i = 0; i < sizeOfArray; i += n)
+    {
+        
+        for(int j = 0; j < n; j++)
+        {
+            tempAnswer[i + j] = my_string[cnt];     
+        } cnt++;      
+    }
+    strcpy(answer, tempAnswer);
+    
+    return answer;
+}
