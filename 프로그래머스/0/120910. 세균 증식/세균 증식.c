04f096369c50a434eaab8079d8de3d87c3d37d98
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-08-19 00:11:19 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.41 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int t) {
+    //n 세균 마리수, t 경과한 시간
+    int answer = 0;
+    
+    answer = n << t;
+    
+    return answer;
+}
