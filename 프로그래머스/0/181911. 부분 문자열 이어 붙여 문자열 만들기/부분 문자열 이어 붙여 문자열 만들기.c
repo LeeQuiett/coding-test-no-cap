96ab9d280497dfdb25d4aea866989f04d6a0e1dc
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-08-19 20:45:51 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.c
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.c
@@ -7,7 +7,15 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* my_strings[], size_t my_strings_len, int** parts, size_t parts_rows, size_t parts_cols) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc(10000);
+    int len = 0;
+    for(int i = 0; i < parts_rows; i++)
+    {
+        int start = parts[i][0];
+        int end = parts[i][1];
+        len = len + (end - start + 1);
+    }
+    
+    char* answer = (char*)malloc(len + 1);
     int cnt = 0;
     printf("%d, %d", parts_rows, parts_cols);
     
