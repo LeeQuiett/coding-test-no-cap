2b2787cfdaa0c71d50a03742f2ff1f1f24b12b91
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-08-20 13:19:45 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+//배고프당
+
+int solution(int n) {
+    int answer = 0;
+    
+    if(n % 2 != 0)
+    {
+        for(int i = 1; i <= n; i += 2)
+        {
+            answer += i;
+        }
+    }
+    else
+    {
+        for(int i = 0; i <= n; i += 2)
+        {
+            answer += (i * i);
+        }
+    }
+    return answer;
+}
