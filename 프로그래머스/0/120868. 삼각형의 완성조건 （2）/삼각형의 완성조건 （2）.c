ba4b0e2c46d92a5ab3aeb6d9722ce2b6282dd44d
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2024-11-19 15:22:00 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 2.45 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// sides_len은 배열 sides의 길이입니다.
+int solution(int sides[], size_t sides_len) {
+    int answer = 0;
+    return answer;
+}
