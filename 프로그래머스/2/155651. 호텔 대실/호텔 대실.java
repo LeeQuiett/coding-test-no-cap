b4c61a5c788b1cca32dc11e58cc9df01e54c6e8b
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2025-02-23 19:20:01 +0900

    [level 2] Title: 호텔 대실, Time: 5.13 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int solution(String[][] book_time) {
+        int answer = 0;
+        int row = book_time.length;
+        int[][] times = new int[row][2];
+        
+        for(int i = 0; i < row; i++) {
+            for (int j = 0; j < 2; j++) {
+                String[] timeParts = book_time[i][j].split(":");
+                int hours = Integer.parseInt(timeParts[0]);
+                int minutes = Integer.parseInt(timeParts[1]);
+                
+                if (j == 1) {
+                    times[i][j] = hours * 60 + minutes + 10; // 청소 시간 고려    
+                } else {
+                    times[i][j] = hours * 60 + minutes;
+                }
+            }
+        }
+        
+        /* for (int[] row2 : times) {
+            for (int col2 : row2) {
+                System.out.print(col2 + " ");
+            } System.out.println();
+        } */
+        
+        int[] timeline = new int[1440 + 10]; //하루는 최대 1440분
+        
+        for (int[] time : times) {
+            int start = time[0];
+            int end = time[1];
+            
+            timeline[start]++;
+            timeline[end]--;
+        }
+        
+        int maxOverlap = 0;
+        int currentOverlap = 0;
+        
+        for (int i = 0; i < 1440; i++) {
+            currentOverlap += timeline[i];
+            maxOverlap = Math.max(currentOverlap, maxOverlap);
+        }
+        return maxOverlap;
+    }
+}
