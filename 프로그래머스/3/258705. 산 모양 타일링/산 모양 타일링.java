9c95afe62c4538838b0bfc2fc2a6c1f332342d44
SangHo.Lee <91045946+LeeQuiett@users.noreply.github.com>
2025-02-25 23:04:59 +0900

    Update 산 모양 타일링.java

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -7,6 +7,7 @@ class Solution {
         dp[0][1] = 0;
         
         for (int i = 0; i < n; i++) {
+            /*연산마다 MOD 연산 안하면 n이 클때 오버플로우가 발생하여 테스트케이스 통과 못함*/
             dp[i + 1][0] = (dp[i][0] * (2 + tops[i]) % MOD + dp[i][1] * (1 + tops[i]) % MOD) % MOD;
             dp[i + 1][1] = (dp[i][0] + dp[i][1]) % MOD;
         }
